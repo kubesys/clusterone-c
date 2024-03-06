115de388f37a3ffce06cfc0dc1515525e12b7970
syswu <wuheng@iscas.ac.cn>
2024-03-06 15:56:11 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/**
+ * Copyright (2024, ) Institute of Software, Chinese Academy of Sciences
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * author: wuheng@iscas.ac.cn
+ * since:  0.1
+ */
+
+#include <iostream>
+#include <boost/date_time/gregorian/gregorian.hpp>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
